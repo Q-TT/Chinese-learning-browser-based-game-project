e1fe6c2925df79b85206fc7b6d52d9fa2704e692
Astrid Ge <qiutongg@umich.edu>
2024-08-09 16:36:53 -0700

    complete all 10 eaning quizes

--- js/app.js
+++ js/app.js
@@ -10,31 +10,96 @@
 //* After 10 quizes, check correct-rate, winCondition is correct-rate >= 60%
 //* If winRate >= 60%, they reveived the message "Congrats! you pass the quizes. Now you can move to next level"
 //! how can I automatically move to the next quiz/page???
-//! how can I check the correct-rate corss all html pages??
+
 
 console.log("check")
 
+let turn = 0 // start from 0, add 1 ecah time users make s seletion
 let winTimes = 0 // when it is >= 6, users win
 let quizList = 
 [{
-    question:"What is the meaning of the following word?",
-    word:"一",
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"一",
     option1:"One",
     option2:"Two",
     option3:"Three",
     option4:"Four",
-    correctOption: "One"},
+    correctOption: "option1"},
 
 {
-    question:"What is the meaning of the following word?",
-    word:"九",
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"九",
     option1:"Two",
     option2:"Five",
     option3:"Nine",
     option4:"Ten",
-    correctOption: "Nine"},
-
+    correctOption: "option3"},
+{
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"四",
+    option1:"Seven",
+    option2:"Four",
+    option3:"Eleven",
+    option4:"Three",
+    correctOption: "option2"},
+{
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"千",
+    option1:"Seven",
+    option2:"Two",
+    option3:"Five",
+    option4:"Thousand",
+    correctOption: "option4"},
+{
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"五",
+    option1:"Five",
+    option2:"Six",
+    option3:"Three",
+    option4:"Ten",
+    correctOption: "option1"},
+{
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"万",
+    option1:"Two thousands",
+    option2:"Ten thousands",
+    option3:"Thirteen",
+    option4:"Six",
+    correctOption: "option2"},
+{
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"八",
+    option1:"Five thousands",
+    option2:"Eight",
+    option3:"Six",
+    option4:"Ten",
+    correctOption: "option2"},
+{
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"七",
+    option1:"Eleven",
+    option2:"Hundred",
+    option3:"Seven",
+    option4:"Two",
+    correctOption: "option3"},
+{
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"三",
+    option1:"Twelve",
+    option2:"Forteen",
+    option3:"Ten",
+    option4:"Three",
+    correctOption: "option4"},
+{
+    questionText:"What is the meaning of the following word?",
+    questionChinese:"六",
+    option1:"Six",
+    option2:"Four",
+    option3:"Two",
+    option4:"Ten",
+    correctOption: "option1"},
 ]
+
 
 
 const optionEls = document.querySelectorAll(".option")
@@ -43,58 +108,78 @@ const optionEls = document.querySelectorAll(".option")
 // const quiz1_Option3 = document.querySelector("#quiz1-option3")
 // const quiz1_Option4 = document.querySelector("#quiz1-option4")
 
-const checkmarkEls = document.querySelectorAll(".checkmark")
+const radioButEls = document.querySelectorAll("input")
 const modalEl = document.querySelector(".moda")
+
 const correctDialogEl = document.querySelector("#correctModal")
 const wrongDialogEl = document.querySelector("#wrongModal")
+
 const correctCloseEl = document.querySelector("#correctClose")
 const wrongCloseEl = document.querySelector("#wrongClose")
 
+const questionTextEl = document.querySelector(".question-text")
+const questionChineseEl = document.querySelector(".question-chinese")
+const option1El = document.querySelector("#option1")
+const option2El = document.querySelector("#option2")
+const option3El = document.querySelector("#option3")
+const option4El = document.querySelector("#option4")
+
 //* init() will let me initialize the first question
 //* update quiz() will be called in init function on first render
-//* 
 
+init()
 function init() {
     updateQuiz()
-
 }
 
+
 function checkCorrect() {
-    if (this.id === "correct") {
+    if (this.value === quizList[turn].correctOption) {
+        console.log("yes!")
         correctDialogEl.showModal()
         winTimes += 1
     } 
-    if(this.id === "wrong") {
+    if(this.value !== quizList[turn].correctOption) {
         wrongDialogEl.showModal()
     }
 }
 //! why the function below doesn't work???
+//! when do we need target, when we don't???
 // function checkCorrect(event) {
 //     if (event.target.id === "correct" ) {
 //         console.log("correct")
 //     }
 // }
 
+// update quz content and clear the check of the radio button
 function updateQuiz() {
-//* update
-
+    questionTextEl.textContent = quizList[turn].questionText
+    questionChineseEl.textContent = quizList[turn].questionChinese
+    option1El.textContent = quizList[turn].option1
+    option2El.textContent = quizList[turn].option2
+    option3El.textContent = quizList[turn].option3
+    option4El.textContent = quizList[turn].option4
+    radioButEls.forEach((radioButEl) => {
+        radioButEl.checked = false
+    })
 }
 
 
-
-
-
-
-checkmarkEls.forEach((checkmarkEl) => {
-    checkmarkEl.addEventListener("click", checkCorrect)
+// check of the selectiois correct when click on the radio button
+radioButEls.forEach((radioButEl) => {
+    radioButEl.addEventListener("click", checkCorrect)
 })
+
 
 // close the dialog by clicking the close button
 correctCloseEl.addEventListener("click", () => {
     correctDialogEl.close()
+    turn += 1
     updateQuiz()
 })
+
 wrongCloseEl.addEventListener("click", () => {
     wrongDialogEl.close()
+    turn += 1
     updateQuiz()
 })
---
+++ js/test.js
@@ -0,0 +1,5 @@
+const option1El = document.querySelector("#huey")
+const option2El = document.querySelector("#dewey")
+const option3El = document.querySelector("#louie")
+
+option1El.textContent = "bbb"

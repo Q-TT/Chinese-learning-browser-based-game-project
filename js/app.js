0ac72e0024e0e56cc0e79494a81443ba4d25a186
Astrid Ge <qiutongg@umich.edu>
2024-08-08 18:53:21 -0700

    set up html css and JS for the first quiz and pop-up dialog

---
+++ js/app.js
@@ -0,0 +1,65 @@
+//* Queryselect all the components that will have interations
+//* Created variable:
+    //* checkCorrect = false
+    //* wins=0
+//* Check each quiz: 
+    //* if the anserr is correct, trigger popup dialog says" you're correct"; 
+    //* if the answer is not correct, trigger popup dialog says "you're incorrect"
+//* Move to next quiz
+//* Add the wrong-answered quiz to the "redo-list"
+//* After 10 quizes, check correct-rate, winCondition is correct-rate >= 60%
+//* If winRate >= 60%, they reveived the message "Congrats! you pass the quizes. Now you can move to next level"
+
+
+//! how can I automatically move to the next quiz/page???
+//! how can I check the correct-rate corss all html pages??
+
+console.log("check")
+
+
+const optionEls = document.querySelectorAll(".option")
+// const quiz1_Option1 = document.querySelector("#quiz1-option1")
+// const quiz1_Option2 = document.querySelector("#quiz1-option2")
+// const quiz1_Option3 = document.querySelector("#quiz1-option3")
+// const quiz1_Option4 = document.querySelector("#quiz1-option4")
+
+const checkmarkEls = document.querySelectorAll(".checkmark")
+const modalEl = document.querySelector(".moda")
+const correctDialogEl = document.querySelector("#correctModal")
+const wrongDialogEl = document.querySelector("#wrongModal")
+const correctCloseEl = document.querySelector("#correctClose")
+const wrongCloseEl = document.querySelector("#wrongClose")
+
+
+checkCorrect()
+
+function checkCorrect() {
+    if (this.id === "correct") {
+        console.log("correct")
+        correctDialogEl.showModal()
+    } 
+    if(this.id === "wrong") {
+        console.log("wrong")
+        wrongDialogEl.showModal()
+    }
+}
+
+//! why the function below doesn't work???
+// function checkCorrect(event) {
+//     if (event.target.id === "correct" ) {
+//         console.log("correct")
+//     }
+// }
+
+checkmarkEls.forEach((checkmarkEl) => {
+    checkmarkEl.addEventListener("click", checkCorrect)
+})
+
+
+// close the dialog by clicking the close button
+correctCloseEl.addEventListener("click", () => {
+    correctDialogEl.close()
+})
+wrongCloseEl.addEventListener("click", () => {
+    wrongDialogEl.close()
+})

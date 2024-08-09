98f7eb87e7f45760d21427c43ff5257dd417218f
Astrid Ge <qiutongg@umich.edu>
2024-08-09 16:39:25 -0700

    complete all 10 eaning quizes

--- js/test.js
+++
@@ -1,5 +0,0 @@
-const option1El = document.querySelector("#huey")
-const option2El = document.querySelector("#dewey")
-const option3El = document.querySelector("#louie")
-
-option1El.textContent = "bbb"
